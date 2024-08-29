9d44dccdacd3de32c5b9f1f0fab43530955b790f
infomotin <motin.mmk.jr@gmail.com>
2024-08-29 15:34:28 +0600

    adding user route api regenarate with group function

--- routes/api/users.php
+++ routes/api/users.php
@@ -19,8 +19,33 @@ use Illuminate\Support\Facades\Route;
 // Route::apiResource('users', UserController::class);
 
 
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{user}', [UserController::class, 'show']);
-Route::post('/users/{user}', [UserController::class, 'store']);
-Route::patch('/users/{user}', [UserController::class, 'update']);
-Route::delete('/users/{user}', [UserController::class, 'destroy']);
+Route::group([
+    'middleware' => [
+        'auth:sanctum',
+    ],
+    'prefix' => 'users',
+    'as' => 'users.',
+    'namespace' => 'App\Http\Controllers',
+
+], function () {
+    Route::get('/users', [UserController::class, 'index'])->name('index');
+    // Route::get('/users', 'UserController@index')->name('index');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('show');
+    Route::post('/users/{user}', [UserController::class, 'store'])->name('store');
+    Route::patch('/users/{user}', [UserController::class, 'update'])->name('update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('destroy');
+});
+
+// Route::middleware('auth')
+//     // ->prefix('api')
+//         ->name('users.')
+//         ->namespace('App\Http\Controllers')
+//         ->group(function () {
+//         Route::get('/users', [UserController::class, 'index'])->name('index');
+//         // Route::get('/users', 'UserController@index')->name('index');
+//         Route::get('/users/{user}', [UserController::class, 'show'])->name('show');
+//         Route::post('/users/{user}', [UserController::class, 'store'])->name('store');
+//         Route::patch('/users/{user}', [UserController::class, 'update'])->name('update');
+//         Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('destroy');
+//     });
+
